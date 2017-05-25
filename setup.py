43eb24b298160ddb448064a27232e96aac44c1a9
Russell Plunkett <russ.plunkett@gmail.com>
2017-05-25 17:04:23 +0100

    First commit - Wilsons and Byars Confidence Interval calculations available.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PublicHealthCIs',
+  packages = ['PublicHealthCIs'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A lib to calculate specific confidence intervals that are used in Public Health',
+  author = 'Russell Plunkett',
+  author_email = 'russ.plunkett@gmail.com',
+  url = 'https://github.com/RustyBrain/PublicHealthCIs.git', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
