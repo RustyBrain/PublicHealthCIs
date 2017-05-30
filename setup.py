7e6c74cd60f77b2eb57230c401f599a8d6411030
Russell Plunkett <russ.plunkett@gmail.com>
2017-05-30 09:39:22 +0100

    Corrected Byars calculation and renamed to python conventions

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'PublicHealthCIs',
-  packages = ['PublicHealthCIs'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A lib to calculate specific confidence intervals that are used in Public Health',
-  author = 'Russell Plunkett',
-  author_email = 'russ.plunkett@gmail.com',
-  url = 'https://github.com/RustyBrain/PublicHealthCIs.git', # use the URL to the github repo
-  download_url = 'https://github.com/RustyBrain/PublicHealthCIs/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['confidence interval', 'wilsons', 'byars', 'statistics'], # arbitrary keywords
-  classifiers = [],
+    name='public-health-cis',
+    packages=['public-health-cis'], # this must be the same as the name above
+    version='0.1',
+    description='A lib to calculate specific confidence intervals that are used in Public Health',
+    author='Russell Plunkett',
+    author_email='russ.plunkett@gmail.com',
+    url='https://github.com/RustyBrain/PublicHealthCIs.git', # use the URL to the github repo
+    download_url='https://github.com/RustyBrain/PublicHealthCIs/archive/0.1.tar.gz', # I'll explain this in a second
+    keywords=['confidence interval', 'wilsons', 'byars', 'statistics'], # arbitrary keywords
+    classifiers=[],
 )
